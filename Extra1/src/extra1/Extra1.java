171fb6a287af5e0523cca1a8887730fb537d2ea4
finfanterodal <finfanterodal@nautilus11.danielcastelao.org>
2018-10-10 12:17:47 +0200

    Extra1Math.

--- Extra1/src/extra1/Extra1.java
+++ Extra1/src/extra1/Extra1.java
@@ -15,9 +15,9 @@ public class Extra1 {
     System.out.println("Introduce el Radio:");
    radio=leer.nextDouble();
     }while(radio<0);
-    System.out.println("La longitud de la circunferencia es = "+(2*PI*radio));
-    System.out.println("\nEl área del círculo es = "+(PI*Math.pow(radio, 2)));
-    System.out.println("\nEl Volumen de la esfera es = "+((4/3)*PI*Math.pow(radio, 3)));
+    System.out.println("La longitud de la circunferencia es = "+Math.round(2*PI*radio));
+    System.out.println("\nEl área del círculo es = "+Math.round(PI*Math.pow(radio, 2)));
+    System.out.println("\nEl Volumen de la esfera es = "+Math.round(4/3*PI*Math.pow(radio, 3)));
     
     }   
 }
